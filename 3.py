b49677f62fd42b9c6e604e0fe746583432498e03
RIP1982 <perovsky@list.ru>
2022-09-11 18:16:07 +0400

    solution for task 3 with corrective

--- 3.py
+++ 3.py
@@ -1,8 +1,13 @@
-# Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+# Задайте список из вещественных чисел. Напишите программу,
+# которая найдёт разницу между максимальным и минимальным значением дробной части элементов.
+#
+# Пример:
+# - [1.1, 1.2, 3.1, 5, 10.01] => 0.19
+some_list = [float(input()) for _ in range(int(input('Введите количество элементов: ')))]
+print(f'some_list = {some_list}')
+new_list = [0 for _ in range(0, len(some_list))]
+for i in range(0, len(some_list)):
+    new_list[i] = round(some_list[i] - int(some_list[i]), 2)
+print(f'new_list[max] - new_list[min] = {max(new_list) - min(new_list)}')
 
-n = int(input('Введите число N: '))
-binary = ''
-while n > 0:
-    binary += str(n%2)
-    n = n//2
-print(binary[::-1])
+
